ea67224365f4a4546d5fa419f576f376f4ad4b7d
Nilesh Patil <67678023+nil0412@users.noreply.github.com>
2022-11-25 17:57:44 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 
+window.addEventListener("load", ()=>{
+    initializeGame();
+});
+
+let stick = document.getElementsByClassName("stick");
+
+var moveStick = 10;
+
+var is_Stick_1_Turn = true;
+
+var ball = document.getElementById("ball");
+
+function initializeGame(){
+    stick[0].style.left = 200 + "px";
+    stick[1].style.left = 200 + "px";
+    if(is_Stick_1_Turn === true){
+        ball.style.top = 0 + "px";
+    }
+    else{
+        ball.style.top = 416 + "px";
+    }
+}
+
+window.addEventListener("keypress", (event)=>{
+    switch(event.key){
+        case "a":
+            if(parseInt(stick[0].style.left)>0){
+                stick[0].style.left = parseInt(stick[0].style.left) - moveStick + "px";
+                stick[1].style.left = parseInt(stick[1].style.left) - moveStick + "px";
+            }
+            break;
+        case "d":
+            if(parseInt(stick[1].style.left)<400){
+                stick[0].style.left = parseInt(stick[0].style.left) + moveStick + "px";
+                stick[1].style.left = parseInt(stick[1].style.left) + moveStick + "px";
+            }
+            break;
+    }
+});
+
+
+
+
